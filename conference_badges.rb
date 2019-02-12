06a0ad657574a81527d3e5dc625707ab1af57211
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-12 18:30:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,5 +3,5 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(speaker_array)
-  array.each{|speaker| "Hello, my name is #{speaker}."
+  array.each{|speaker| "Hello, my name is #{speaker}.}"
 end
